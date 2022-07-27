be40220636e87744e0e1addaecbaede3de632cd7
Jian Yu <yujian6231@github.com>
2022-07-27 18:04:21 +0200

    test

---
+++ README.md
@@ -0,0 +1,104 @@
+# 3D Bounding Box Estimation Using Deep Learning and Geometry
+
+ 
+## Prerequisites
+1. TensorFlow-gpu 1.14.0
+2. Numpy
+3. OpenCV
+4. tqdm
+
+## Installation
+1. Clone the repository
+   ```Shell
+   git clone https://github.com/smallcorgi/3D-Deepbox.git
+   ```
+2. Download the KITTI object detection dataset, calib and label (http://www.cvlibs.net/datasets/kitti/eval_object.php?obj_benchmark=2d).
+3. Download the weights file (vgg_16.ckpt).
+   ```Shell
+   cd $3D-Deepbox_ROOT
+   wget http://download.tensorflow.org/models/vgg_16_2016_08_28.tar.gz
+   tar zxvf vgg_16_2016_08_28.tar.gz
+   ```
+
+4. Download the weights file (yolov3_coco)
+   ```Shell
+   mkdir checkpoint
+   cd checkpoint
+   wget https://github.com/YunYang1994/tensorflow-yolov3/releases/download/v1.0/yolov3_coco.tar.gz
+   tar -xvf yolov3_coco.tar.gz
+   cd ..
+   python convert_weight.py
+   python freeze_graph.py
+   ```
+
+5. Download the weights file (model.tar.gz)
+ ```Shell
+   mkdir model
+   cd model
+   download model folder from google docs:
+   https://drive.google.com/file/d/1yihIYsEoqc3NwDlPkZKeGsCcN_Y_J-QW/view?usp=sharing
+   tar -xvf model.tar.gz
+   ```
+
+
+
+## Usage
+
+### Train model
+   ```Shell
+   python main.py --mode train --gpu [gpu_id] --image [train_image_path] --label [train_label_path] --box2d [train_2d_boxes]
+   ```
+
+### Test model
+   ```Shell
+   python main.py --mode test --gpu [gpu_id] --image [test_image_path] --box2d [test_2d_boxes_path] --model [model_path] --output [output_file_path]
+
+   For testing image:
+   eg:
+   python main_image.py --mode test --image ./docs/images/
+   (CALIBRATION FILE MUST BE PUT INTO ./docs/cal/)
+
+   For testing video:
+   video_path and calib_path must be changed to where you save the files
+   eg:
+   video_path = "./docs/images/2022-06-09-21-38-59_usb_cam_image_raw.mp4"
+   calib_path = os.path.abspath(os.path.dirname(__file__)) + "/" + 'docs/cal/realsensor.txt'
+   python main_video.py 
+   ```
+
+
+
+### Result
+
+car_detection AP: 100.000000 100.000000 100.000000
+
+car_orientation AP: 98.482552 95.809959 91.975380
+
+pedestrian_detection AP: 100.000000 100.000000 100.000000
+
+pedestrian_orientation AP: 76.835083 74.943863 71.997620
+
+cyclist_detection AP: 100.000000 100.000000 100.000000
+
+cyclist_orientation AP: 89.908524 81.029915 79.436340
+
+car_detection_ground AP: 90.743927 85.268692 76.673523
+
+pedestrian_detection_ground AP: 97.148033 98.034355 98.376617
+
+cyclist_detection_ground AP: 82.906242 82.897720 75.573006
+
+Eval 3D bounding boxes
+
+car_detection_3d AP: 84.500374 84.358612 75.764938
+
+pedestrian_detection_3d AP: 96.662766 97.702209 89.280357
+
+cyclist_detection_3d AP: 80.711548 81.337944 74.269547
+
+
+## References
+1. https://github.com/shashwat14/Multibin
+2. https://github.com/experiencor/didi-starter/tree/master/simple_solution
+3. https://github.com/experiencor/image-to-3d-bbox
+4. https://github.com/prclibo/kitti_eval
---
+++ main_video.py
@@ -0,0 +1,381 @@
+import tensorflow as tf
+import tensorflow.contrib.slim as slim
+import cv2
+import os
+import numpy as np
+import time
+from random import shuffle
+from data_processing import *
+import sys
+import argparse
+from tqdm import tqdm
+
+from image_demo import *
+
+# show result
+from library.Math import *
+from library.Plotting import *
+
+from core.utils import *
+
+# time count
+import time
+
+#####
+# Training setting
+
+BIN, OVERLAP = 2, 0.1
+W = 1.
+ALPHA = 1.
+MAX_JIT = 3
+NORM_H, NORM_W = 224, 224
+VEHICLES = ['Car', 'Truck', 'Van', 'Tram', 'Pedestrian', 'Cyclist']
+BATCH_SIZE = 8
+learning_rate = 0.0001
+epochs = 50
+save_path = './model/'
+
+dims_avg = {'Cyclist': np.array([1.73532436,  0.58028152,  1.77413709]), 'Van': np.array([2.18928571,  1.90979592,  5.07087755]), 'Tram': np.array([3.56092896,   2.39601093,  18.34125683]), 'Car': np.array([
+    1.52159147,  1.64443089,  3.85813679]), 'Pedestrian': np.array([1.75554637,  0.66860882,  0.87623049]), 'Truck': np.array([3.07392252,   2.63079903,  11.2190799])}
+
+
+# Placeholder
+inputs = tf.placeholder(tf.float32, shape=[None, 224, 224, 3])
+d_label = tf.placeholder(tf.float32, shape=[None, 3])
+o_label = tf.placeholder(tf.float32, shape=[None, BIN, 2])
+c_label = tf.placeholder(tf.float32, shape=[None, BIN])
+
+
+def parse_args():
+    """Parse input arguments."""
+    parser = argparse.ArgumentParser(description='3D bounding box')
+    parser.add_argument('--mode', dest='mode',
+                        help='train or test', default='test')
+    parser.add_argument('--video', dest='video',
+                        help='Image path', default='./docs/video/')
+    parser.add_argument('--label', dest='label',
+                        help='Label path', default='./training/label_2/')
+    parser.add_argument('--box2d', dest='box2d',
+                        help='2D detection path', default='./training/label_2/')
+    parser.add_argument('--output', dest='output',
+                        help='Output path', default='./validation/result_2/')
+    parser.add_argument('--model', dest='model', default='./model/')
+    parser.add_argument('--gpu', dest='gpu', default='0')
+    args = parser.parse_args()
+
+    return args
+
+
+def build_model():
+
+    # build some layer
+    def LeakyReLU(x, alpha):
+        return tf.nn.relu(x) - alpha * tf.nn.relu(-x)
+
+    def orientation_loss(y_true, y_pred):
+        # Find number of anchors
+        anchors = tf.reduce_sum(tf.square(y_true), axis=2)
+        anchors = tf.greater(anchors, tf.constant(0.5))
+        anchors = tf.reduce_sum(tf.cast(anchors, tf.float32), 1)
+
+        # Define the loss
+        loss = (y_true[:, :, 0]*y_pred[:, :, 0] +
+                y_true[:, :, 1]*y_pred[:, :, 1])
+        loss = tf.reduce_sum((2 - 2 * tf.reduce_mean(loss, axis=0))) / anchors
+
+        return tf.reduce_mean(loss)
+
+    #####
+    # Build Graph
+    with slim.arg_scope([slim.conv2d, slim.fully_connected],
+                        activation_fn=tf.nn.relu,
+                        weights_initializer=tf.truncated_normal_initializer(
+                            0.0, 0.01),
+                        weights_regularizer=slim.l2_regularizer(0.0005)):
+        net = slim.repeat(inputs, 2, slim.conv2d, 64, [3, 3], scope='conv1')
+        net = slim.max_pool2d(net, [2, 2], scope='pool1')
+        net = slim.repeat(net, 2, slim.conv2d, 128, [3, 3], scope='conv2')
+        net = slim.max_pool2d(net, [2, 2], scope='pool2')
+        net = slim.repeat(net, 3, slim.conv2d, 256, [3, 3], scope='conv3')
+        net = slim.max_pool2d(net, [2, 2], scope='pool3')
+        net = slim.repeat(net, 3, slim.conv2d, 512, [3, 3], scope='conv4')
+        net = slim.max_pool2d(net, [2, 2], scope='pool4')
+        net = slim.repeat(net, 3, slim.conv2d, 512, [3, 3], scope='conv5')
+        net = slim.max_pool2d(net, [2, 2], scope='pool5')
+        conv5 = tf.contrib.layers.flatten(net)
+
+        #dimension = slim.fully_connected(conv5, 512, scope='fc7_d')
+        dimension = slim.fully_connected(
+            conv5, 512, activation_fn=None, scope='fc7_d')
+        dimension = LeakyReLU(dimension, 0.1)
+        dimension = slim.dropout(dimension, 0.5, scope='dropout7_d')
+        #dimension = slim.fully_connected(dimension, 3, scope='fc8_d')
+        dimension = slim.fully_connected(
+            dimension, 3, activation_fn=None, scope='fc8_d')
+        #dimension = LeakyReLU(dimension, 0.1)
+
+        #loss_d = tf.reduce_mean(tf.square(d_label - dimension))
+        loss_d = tf.losses.mean_squared_error(d_label, dimension)
+
+        #orientation = slim.fully_connected(conv5, 256, scope='fc7_o')
+        orientation = slim.fully_connected(
+            conv5, 256, activation_fn=None, scope='fc7_o')
+        orientation = LeakyReLU(orientation, 0.1)
+        orientation = slim.dropout(orientation, 0.5, scope='dropout7_o')
+        #orientation = slim.fully_connected(orientation, BIN*2, scope='fc8_o')
+        orientation = slim.fully_connected(
+            orientation, BIN*2, activation_fn=None, scope='fc8_o')
+        #orientation = LeakyReLU(orientation, 0.1)
+        orientation = tf.reshape(orientation, [-1, BIN, 2])
+        orientation = tf.nn.l2_normalize(orientation, dim=2)
+        loss_o = orientation_loss(o_label, orientation)
+
+        #confidence = slim.fully_connected(conv5, 256, scope='fc7_c')
+        confidence = slim.fully_connected(
+            conv5, 256, activation_fn=None, scope='fc7_c')
+        confidence = LeakyReLU(confidence, 0.1)
+        confidence = slim.dropout(confidence, 0.5, scope='dropout7_c')
+        confidence = slim.fully_connected(
+            confidence, BIN, activation_fn=None, scope='fc8_c')
+        loss_c = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(
+            labels=c_label, logits=confidence))
+
+        confidence = tf.nn.softmax(confidence)
+        #loss_c = tf.reduce_mean(tf.square(c_label - confidence))
+        #loss_c = tf.losses.mean_squared_error(c_label, confidence)
+
+        total_loss = 4. * loss_d + 8. * loss_o + loss_c
+        optimizer = tf.train.GradientDescentOptimizer(
+            learning_rate).minimize(total_loss)
+
+        return dimension, orientation, confidence, total_loss, optimizer, loss_d, loss_o, loss_c
+
+
+def train(image_dir, box2d_loc, label_dir):
+
+    # load data & gen data
+    all_objs = parse_annotation(label_dir, image_dir)
+    all_exams = len(all_objs)
+    np.random.shuffle(all_objs)
+    train_gen = data_gen(image_dir, all_objs, BATCH_SIZE)
+    train_num = int(np.ceil(all_exams/BATCH_SIZE))
+
+    # buile graph
+    dimension, orientation, confidence, loss, optimizer, loss_d, loss_o, loss_c = build_model()
+
+    # GPU config
+    tfconfig = tf.ConfigProto(allow_soft_placement=True)
+    tfconfig.gpu_options.allow_growth = True
+    sess = tf.Session(config=tfconfig)
+
+    # create a folder for saving model
+    if os.path.isdir(save_path) == False:
+        os.mkdir(save_path)
+    variables_to_restore = slim.get_variables()[:26]  # vgg16-conv5
+
+    saver = tf.train.Saver(max_to_keep=100)
+
+    # Load pretrain VGG model
+    ckpt_list = tf.contrib.framework.list_variables(
+        './vgg_16_2016_08_28/vgg_16.ckpt')[1:-7]
+    new_ckpt_list = []
+    for name in range(1, len(ckpt_list), 2):
+        tf.contrib.framework.init_from_checkpoint(
+            './vgg_16_2016_08_28/vgg_16.ckpt', {ckpt_list[name-1][0]: variables_to_restore[name]})
+        tf.contrib.framework.init_from_checkpoint(
+            './vgg_16_2016_08_28/vgg_16.ckpt', {ckpt_list[name][0]: variables_to_restore[name-1]})
+
+    # Initializing the variables
+    init = tf.global_variables_initializer()
+    sess.run(init)
+
+    # Start to train model
+    for epoch in range(epochs):
+        epoch_loss = np.zeros((train_num, 1), dtype=float)
+        tStart_epoch = time.time()
+        batch_loss = 0.0
+        for num_iters in tqdm(range(train_num), ascii=True, desc='Epoch '+str(epoch+1)+' : Loss:'+str(batch_loss)):
+            train_img, train_label = next(train_gen)
+            _, batch_loss = sess.run([optimizer, loss], feed_dict={
+                                     inputs: train_img, d_label: train_label[0], o_label: train_label[1], c_label: train_label[2]})
+
+            epoch_loss[num_iters] = batch_loss
+
+        # save model
+        if (epoch+1) % 5 == 0:
+            saver.save(sess, save_path+"model", global_step=epoch+1)
+
+        # Print some information
+        print("Epoch:", epoch+1, " done. Loss:", np.mean(epoch_loss))
+        tStop_epoch = time.time()
+        print("Epoch Time Cost:", round(tStop_epoch - tStart_epoch, 2), "s")
+        sys.stdout.flush()
+
+
+def calc_theta_ray(img, box_2d, proj_matrix):
+
+    width = img.shape[1]
+    fovx = 2 * np.arctan(width / (2 * proj_matrix[0][0]))
+    center = (box_2d['xmin']+box_2d['xmax']) / 2
+    dx = center - (width / 2)
+
+    mult = 1
+    if dx < 0:
+        mult = -1
+    dx = abs(dx)
+    angle = np.arctan((2*dx*np.tan(fovx/2)) / width)
+    angle = angle * mult
+
+    return angle
+
+
+def plot_regressed_3d_bbox(img, cam_to_img, box_2d, dimensions, alpha, theta_ray, img_2d=None):
+    theta_ray = calc_theta_ray(img, box_2d, cam_to_img)
+    orient = alpha + theta_ray
+    # the math! returns X, the corners used for constraint
+    location, X = calc_location(
+        dimensions, cam_to_img, box_2d, alpha, theta_ray)
+    # if ((-6) < location[0] < (6)) and location[2]<80:
+    if location[2] < 80:
+        plot_3d_box(img, cam_to_img, orient, dimensions, location)  # 3d boxes
+
+        return location
+
+
+def bbox_3d(model, img, bboxes, calib_path):
+    time_start_3dbox = time.time()
+    img_plot = np.copy(img)
+    img = img.astype(np.float32, copy=False)
+
+    for box in bboxes:
+        time_start_perbox = time.time()
+        # detected bbox:
+        obj = {'xmin': (int(float(box[0]))),
+               'ymin': (int(float(box[1]))),
+               'xmax': (int(float(box[2]))),
+               'ymax': (int(float(box[3]))),
+               }
+        # detected class:
+        class_load = read_class_names(cfg.YOLO.CLASSES)
+        class_num = int(float(box[5]))
+
+        patch = img[obj['ymin']:obj['ymax'], obj['xmin']:obj['xmax']]
+        patch = cv2.resize(patch, (NORM_H, NORM_W))
+        patch = patch - np.array([[[103.939, 116.779, 123.68]]])
+        patch = np.expand_dims(patch, 0)
+        prediction = sess.run(
+            [dimension, orientation, confidence], feed_dict={inputs: patch})
+        # Transform regressed angle
+        max_anc = np.argmax(prediction[2][0])
+        anchors = prediction[1][0][max_anc]
+
+        if anchors[1] > 0:
+            angle_offset = np.arccos(anchors[0])
+        else:
+            angle_offset = -np.arccos(anchors[0])
+
+        wedge = 2.*np.pi/BIN
+        angle_offset = angle_offset + max_anc*wedge
+        angle_offset = angle_offset % (2.*np.pi)
+
+        angle_offset = angle_offset - np.pi/2
+        if angle_offset > np.pi:
+            angle_offset = angle_offset - (2.*np.pi)
+
+        # specific to interested objects:
+        if class_load[class_num] in VEHICLES:
+            print("class:", class_load[class_num])
+
+            # Transform regressed dimension
+            if class_load[class_num] in VEHICLES:
+                dims = dims_avg[class_load[class_num]] + prediction[0][0]
+            else:
+                dims = dims_avg['Car'] + prediction[0][0]
+
+            proj_matrix = get_calibration_cam_to_image(calib_path)
+
+            location = plot_regressed_3d_bbox(
+                img_plot, proj_matrix, obj, list(dims), angle_offset, angle_offset)
+            print('Estimated pose: %s' % location)
+
+        time_end_perbox = time.time()
+        print('3dbbox per box time cost:', time_end_perbox-time_start_perbox, 's')
+    time_end_3dbox = time.time()
+    cv2.imshow('3D detections', img_plot)
+    print('3dbbox time cost totally:', time_end_3dbox - time_start_3dbox, 's')
+
+if __name__ == "__main__":
+    # init:
+    args = parse_args()
+    os.environ['CUDA_VISIBLE_DEVICES'] = args.gpu
+
+    if args.model is None:
+        raise IOError(('Model not found.'.format(args.model)))
+
+    # initializing for 3d_bbox:
+    # buile graph
+    dimension, orientation, confidence, loss, optimizer, loss_d, loss_o, loss_c = build_model()
+    tfconfig = tf.ConfigProto(allow_soft_placement=True)
+    tfconfig.gpu_options.allow_growth = True
+    sess = tf.Session(config=tfconfig)
+
+    # Initializing the variablestxt
+    init = tf.global_variables_initializer()
+    sess.run(init)
+
+    # Restore model
+    saver = tf.train.Saver()
+    model = tf.train.latest_checkpoint(args.model)
+
+    saver.restore(sess, model)
+
+    # initializing for 2d_bbox yolo:
+    return_elements = ["input/input_data:0", "pred_sbbox/concat_2:0",
+                       "pred_mbbox/concat_2:0", "pred_lbbox/concat_2:0"]
+    pb_file = "./checkpoint/yolov3_coco.pb"
+    graph = tf.Graph()
+    return_tensors = utils.read_pb_return_tensors(
+        graph, pb_file, return_elements)
+
+    video_path = "./docs/images/2022-06-09-21-38-59_usb_cam_image_raw.mp4"
+    #video_path     = 0
+    calib_path = os.path.abspath(os.path.dirname(__file__)) + "/" + 'docs/cal/realsensor.txt'
+
+    with tf.Session(graph=graph) as sess_2d:
+
+        vid = cv2.VideoCapture(video_path)
+        while True:
+            return_value, frame = vid.read()
+            if return_value:
+                # = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+                image = Image.fromarray(frame)
+            else:
+                raise ValueError("No image!")
+            # FPS:
+            time_start = time.time()
+
+            num_classes = 80
+            input_size = 416
+            frame_size = frame.shape[:2]
+            image_data = utils.image_preporcess(np.copy(frame), [input_size, input_size])
+            image_data = image_data[np.newaxis, ...]
+
+            # pred_box:
+            pred_sbbox, pred_mbbox, pred_lbbox = sess_2d.run(
+                [return_tensors[1], return_tensors[2], return_tensors[3]], feed_dict={return_tensors[0]: image_data})
+            pred_bbox = np.concatenate([np.reshape(pred_sbbox, (-1, 5 + num_classes)),
+                                        np.reshape(pred_mbbox, (-1, 5 + num_classes)),
+                                        np.reshape(pred_lbbox, (-1, 5 + num_classes))], axis=0)
+
+            bboxes = utils.postprocess_boxes(pred_bbox, frame_size, input_size, 0.3)
+            bboxes = utils.nms(bboxes, 0.45, method='nms')
+            time_end_2d = time.time()
+            print('2d bbox time cost:', time_end_2d-time_start, 's')
+
+            bbox_3d(args.model, frame, bboxes, calib_path)
+
+            time_end = time.time()
+            print('time cost totally', time_end-time_start, 's')
+            print('-------------')
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
